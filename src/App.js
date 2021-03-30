955c0074a2ed3bc891f6703736831633b7ef01b0
kaguya-ma <lulamae1983@gmail.com>
2021-03-30 21:02:42 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+
+const fortuneTeller = () => {
+    const firstArray = ["Today is perfect for new endeavours. ", "The tensions of this week will feel heavier today than yesterday. ", 'Today is the day to cherish and embrace others. ', "Making yourself useful is a main component of a successful day. ", "Today, exercise caution when crossing the street. "]
+    const secondArray = ["Remember that good things come to those who work hard. ", "Don’t let the circumstances bring you down. ", "Patience is key, but sometimes a little push can get the job done. ", "A smile can get you a long way. "]
+    const thirdArray = ["Looking ahead may seem like a waste of time, but it pays off in the end. ", "Luck favours those who mind the risks and take them. ", "Today is the day for that thing you always wanted to do. ", "Luck is on your side today, so seize it! ", "Things are looking up for you! "]
+
+
+    const firstArrayIndex = Math.floor(Math.random() * firstArray.length)
+    const firstSentence = firstArray[firstArrayIndex]
+
+    const secondArrayIndex = Math.floor(Math.random() * secondArray.length)
+    const secondSentence = secondArray[secondArrayIndex]
+
+    const thirdArrayIndex = Math.floor(Math.random() * thirdArray.length)
+    const thirdSentence = thirdArray[thirdArrayIndex]
+    document.querySelector('p').innerText=`${firstSentence}${secondSentence}${thirdSentence}`
+
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>Fortune Teller</h1>
+          <p>Check your future</p>
+          <button onClick={fortuneTeller}>Prediction</button>
+      </div>
+
   );
 }
 
